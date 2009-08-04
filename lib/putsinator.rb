58d747cf555fb010aee2a4433e4602d31af520a9
Toby <toby.tripp+git@gmail.com>
2009-08-04 12:49:08 -0500

    Catch calls to 'p' as well

--- lib/putsinator.rb
+++ lib/putsinator.rb
@@ -3,9 +3,19 @@ module Kernel
     file, line = caller[0].split(':')
     file = File.basename file
     
-    default_puts "[#{file}:#{line}]", args
+    default_puts "[#{file}:#{line}]", *args
   end
   
   alias_method :default_puts, :puts
   alias_method :puts, :noisy_puts
+  
+  def noisy_p( *args )
+    file, line = caller[0].split(':')
+    file = File.basename file
+    
+    default_p "[#{file}:#{line}]", *args
+  end
+  
+  alias_method :default_p, :p
+  alias_method :p, :noisy_p
 end
