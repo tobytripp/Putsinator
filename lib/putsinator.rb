e353d68b935770696fa6f3b3704278f9e072be1a
Toby <toby.tripp+git@gmail.com>
2009-08-04 12:00:31 -0500

    Update README ... a bit

--- lib/putsinator.rb
+++ lib/putsinator.rb
@@ -2,8 +2,10 @@ module Kernel
   def noisy_puts( *args )
     file, line = caller[0].split(':')
     file = File.basename file
+    
     default_puts "[#{file}:#{line}]", args
   end
+  
   alias_method :default_puts, :puts
   alias_method :puts, :noisy_puts
 end
--- tasks/putsinator_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :putsinator do
-#   # Task goes here
-# end
