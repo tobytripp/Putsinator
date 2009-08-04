194b6859c0e60d273cb1f8f7449f8230d03b66cf
Toby <toby.tripp+git@gmail.com>
2009-08-04 11:04:34 -0500

    Add a test

--- test/putsinator_test.rb
+++ test/putsinator_test.rb
@@ -1,8 +1,30 @@
 require 'test_helper'
+require "stringio"
+
+module Kernel
+  def capture_stdout
+    out = StringIO.new
+    $stdout = out
+    yield
+    return out
+  ensure
+    $stdout = STDOUT
+  end
+end
 
 class PutsinatorTest < ActiveSupport::TestCase
-
-  test "" do
-    assert true
+  def setup
+    @out
+  end
+  
+  test "that 'puts' fingers file that did it" do
+    stringy = "I'm putsing from a test whoooo"
+    line = 0
+    
+    out = capture_stdout do
+      line = __LINE__; puts stringy
+    end
+    
+    assert_equal "[putsinator_test.rb:#{line}] #{stringy}", out
   end
 end
